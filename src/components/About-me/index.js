39ccf4eb9396f37ba8edb800d1313e5551be3f5f
Darobinson20 <Darobinson20@gmail.com>
2022-07-17 23:04:14 -0400

    About me/port started

---
+++ src/components/About-me/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section className="my-5">
+      <h1 id="about">About Me</h1>
+      
+      <div className="my-2">
+        <p>
+        Hello, My name is Darrin Robinson. I am a full stack developer...in development. Through this bootcamp, I have gained knowledge in many different front end/back end languages, and databases. Although I have no professional experience as a developer , I will continue to make strides to learn all that I can about development. My extensive experience in retail and banking have shaped me into being the hard-working, eager person that I am today. 
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
